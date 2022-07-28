befd6cb79315f13f924b6de73eb258ffdb2ea00b
cldborges <cldborges@yahoo.com.br>
2022-07-28 12:12:56 -0300

    Criado calcular horas e 1a versão operacional

--- funcoes.py
+++ funcoes.py
@@ -11,3 +11,19 @@ def ler_credenciais():
     with open('conf.txt', 'r', encoding='utf-8') as arquivo:
         usuario, senha = arquivo.readlines()
     return usuario, senha
+
+def calcular_horas():
+    from datetime import datetime, timedelta
+    import random
+
+    hora_inicial_minima = '07:55'
+    hora_final_minima = '16:55'
+    horas = []
+    for hora in hora_inicial_minima, hora_final_minima:
+        x = random.randint(0,10)
+        hora1 = datetime.strptime(hora, '%H:%M')
+        hora2 = hora1 + timedelta(minutes=x)
+        Tempofinal_str = hora2.strftime('%H:%M')
+        horas.append(Tempofinal_str)
+    print(horas)
+    return horas
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+from datetime import date
 
-from funcoes import ler_credenciais, trocar_senha
+from funcoes import calcular_horas, ler_credenciais, trocar_senha
 
 url = 'https://seniorx.myatos.net:8181/gestaoponto-frontend/login'
 try:
@@ -15,27 +16,34 @@ except:
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get(url)
+
+#Efetuar login
 login = driver.find_element(By.ID, 'index-vm-username')
 login.send_keys(usuario)
 password = driver.find_element(By.ID, 'index-vm-password')
 password.send_keys(senha)
+time.sleep(2)
 driver.find_element(By.ID, 'index-1500385519648').click()
 
 time.sleep(5)
 
+#Apontar hora
 #linha_hoje = driver.find_element(By.ID, 'linha-0')
-driver.find_element(By.ID, 'dia_2022-07-26_InserirMarcacao').click()
+hoje = date.today()
+hora_inicio, hora_fim = calcular_horas()
+time.sleep(2)
+driver.find_element(By.ID, f'dia_{hoje}_InserirMarcacao').click()
 driver.find_element(By.ID, 'addMarcacao').click()
 marcacao0 = driver.find_element(By.ID, 'marcacaoTime-0')
-marcacao0.send_keys('08:01')
+marcacao0.send_keys(hora_inicio)
 driver.find_element(By.ID, 'selectJustificative-0').click()
-time.sleep(10)
+time.sleep(6)
 driver.find_element(By.ID, 'ui-select-choices-row-1-3').click()
 driver.find_element(By.ID, 'addMarcacao').click()
 marcacao1 = driver.find_element(By.ID, 'marcacaoTime-1')
-marcacao1.send_keys('17:01')
+marcacao1.send_keys(hora_fim)
 driver.find_element(By.ID, 'selectJustificative-1').click()
-time.sleep(10)
-driver.find_element(By.ID, 'ui-select-choices-row-3-3').click()
+time.sleep(6)
+driver.find_element(By.ID, 'saveAppointment').click()
 
 time.sleep(100)
